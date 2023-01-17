31fb0ee50c47f380775854c3f96063dfafffdc84
wiatrogon <wiatrogon@gmail.com>
2023-01-17 19:51:38 +0100

    more test cases for validator

--- tests/test_validator.py
+++ tests/test_validator.py
@@ -27,8 +27,23 @@ def test_two_characters_invalid_roman_numerals(number):
     assert is_valid(number) is False
 
 
-@pytest.mark.parametrize("number", ["VII", "XII", "XIV", "XVI", "XIX", "XXI",
-                                    "XXV", "XXX", "XLI", "XLV", "LII", "LIV",
-                                    "LVI", "LIX", "LXI", "XCI", "XCV"])
+@pytest.mark.parametrize("number", ["III", "VII", "XII", "XIV", "XVI", "XIX",
+                                    "XXI", "XXV", "XXX", "XLI", "XLV", "LII",
+                                    "LIV", "LVI", "LIX", "LXI", "XCI", "XCV",
+                                    "CII", "CIV", "CVI", "CIX", "CXI", "CXV",
+                                    "CXL", "CLI", "CLV", "CLX", "CXC", "CCI",
+                                    "CCV", "CCX", "CCL", "CCC", "CDI", "CDV",
+                                    "CDX", "CDL", "DII", "DIV", "DVI", "DIX",
+                                    "DXI", "DXL", "DLI", "DLV", "DLX", "DXC",
+                                    "DCI", "DCV", "DCX", "DCL", "CMI", "CMV",
+                                    "CMX", "CML", "MMI", "MMV", "MMX", "MML",
+                                    "MMC", "MMD", "MMM"])
 def test_three_characters_valid_roman_numerals(number):
     assert is_valid(number) is True
+
+
+@pytest.mark.parametrize("number", ["IIV", "IIX", "IIL", "IIC", "IID", "IIM",
+                                    "IVI", "IXI", "ILI", "ICI", "IDI", "IMI",
+                                    "VVI", "LLI", "LLX", "DDI", "DDX", "DDC"])
+def test_three_characters_invalid_roman_numerals(number):
+    assert is_valid(number) is False
