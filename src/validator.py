cf01a8ce04f0823e6923202cf4a0c2aef865e122
wiatrogon <wiatrogon@gmail.com>
2023-01-17 19:32:26 +0100

    improved validator implementation

--- src/validator.py
+++ src/validator.py
@@ -2,6 +2,6 @@ import re
 
 
 def is_valid(number: str) -> bool:
-    pattern = re.compile(r"[MDCLXVI]+")
+    pattern = re.compile(r"^(C[MD]|M*D?C{0,3})(X[CL]|L?X{0,3})(I[XV]|V?I{0,3})$")
 
     return bool(pattern.match(number))
--- tests/test_validator.py
+++ tests/test_validator.py
@@ -3,11 +3,23 @@ import pytest
 from src.validator import is_valid
 
 
+@pytest.mark.parametrize("number", ["FOO", "BAR"])
+def test_invalid_characters(number):
+    assert is_valid(number) is False
+
+
 @pytest.mark.parametrize("number", ["I", "V", "X", "L", "C", "D", "M"])
 def test_single_character_valid_roman_numeral(number):
     assert is_valid(number) is True
 
 
-@pytest.mark.parametrize("number", ["IV", "VI", "IX", "XI", "XL", "LI", "LV", "LX", "XC", "CI", "CV", "CD", "CM"])
+@pytest.mark.parametrize("number", ["IV", "VI", "IX", "XI", "XL", "LI", "LV",
+                                    "LX", "XC", "CI", "CV", "CD", "CM"])
 def test_two_characters_valid_roman_numerals(number):
     assert is_valid(number) is True
+
+
+@pytest.mark.parametrize("number", ["IL", "IC", "ID", "IM", "VX", "VL", "VC",
+                                    "VD", "VM", "XD", "XM", "LC", "LD", "LM", "DM"])
+def test_two_characters_invalid_roman_numerals(number):
+    assert is_valid(number) is False
